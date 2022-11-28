b77737d4b8c01044e16ecbfaf5b74d705fc24d57
Diana Damian <diana.damiaan@gmail.com>
2022-11-28 15:41:31 +0200

    Side navbar mobile responsive

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const filterButton = document.querySelector('#filterButton');
 const filteringForm = document.querySelector('.filtering-form');
+const navButton = document.querySelector('#navButton');
+const sideNavBar = document.querySelector('.side-navbar');
 
 filterButton.addEventListener("click", function () {
     filteringForm.classList.toggle('hidden');
     filterButton.classList.toggle('is--open');
 });
+
+navButton.addEventListener("click", function () {
+    sideNavBar.classList.toggle('side-navbar--open');
+})
